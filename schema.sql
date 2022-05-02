4efe3b47e6b8f02bc3f731ad2c79e122853ea036
Tomas Garza <99302916+tdgarza@users.noreply.github.com>
2022-05-02 18:59:13 -0500

    Actualizando el schema

--- schema.sql
+++ schema.sql
@@ -19,3 +19,19 @@ create table post_category(
 	foreign key (post_id) references post(id),
 	foreign key (category_id) references category(id)
 );
+
+create database avengers;
+use avengers;
+
+create table status(
+	idstatus int not null auto_increment primary key,
+	status varchar(50)
+  );
+create table personajes(
+	id int not null auto_increment primary key,
+	nombre varchar(50),
+	nombrereal varchar(50),
+	poderes varchar(50),
+	primeraaparicion varchar(50),
+	foreign key (id) references status(idstatus)
+);
