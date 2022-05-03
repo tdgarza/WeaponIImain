cde222cdeeb19c2a9f5be06749782317712ce989
Tomas Garza <99302916+tdgarza@users.noreply.github.com>
2022-05-03 13:37:50 -0500

    Subir cambios 2

--- schema.sql
+++ schema.sql
@@ -35,3 +35,34 @@ create table personajes(
 	primeraaparicion varchar(50),
 	foreign key (id) references status(idstatus)
 );
+
+
+CREATE TABLE notas_estudiantes_materias(
+    id bigint unsigned not null primary key auto_increment,
+    id_estudiante bigint unsigned not null,
+    id_materia bigint unsigned not null,
+    puntaje decimal(9,2) not null,
+    foreign key (id_estudiante) references estudiantes(id) on delete cascade on update cascade,
+    foreign key (id_materia) references materias(id) on delete cascade on update cascade
+);
+
+CREATE TABLE estudiantes(
+    id bigint unsigned not null primary key auto_increment,
+    nombre varchar(255) not null,
+    grupo varchar(255) not null
+);
+
+CREATE TABLE materias(
+    id bigint unsigned not null primary key auto_increment,
+    nombre varchar(255) not null
+);
+
+
+CREATE TABLE notas_estudiantes_materias(
+    id bigint unsigned not null primary key auto_increment,
+    id_estudiante bigint unsigned not null,
+    id_materia bigint unsigned not null,
+    puntaje decimal(9,2) not null,
+    foreign key (id_estudiante) references estudiantes(id) on delete cascade on update cascade,
+    foreign key (id_materia) references materias(id) on delete cascade on update cascade
+);
